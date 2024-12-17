2e23beb297b2558519fbaec4ec014979d01ffb8c
2Smean <136636643+2Smean@users.noreply.github.com>
2024-12-17 19:32:23 +0900

    add : Component scanning and automatic dependency setup

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 공유되는 변수일 때 ConcurrentHashMap 을 사용
