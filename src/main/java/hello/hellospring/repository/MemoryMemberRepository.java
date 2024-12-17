07e4337f0280c4e2f4876fc656ec99875291763c
2Smean <136636643+2Smean@users.noreply.github.com>
2024-12-17 19:44:15 +0900

    add : Registering Spring beans directly with Java code

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 공유되는 변수일 때 ConcurrentHashMap 을 사용
